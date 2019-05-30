3947525f0c80b5ae59375b1f97f6617151d8e7f2
JesusCanales <jesuscanales@outlook.com>
2019-05-30 02:06:05 -0500

    agregue registros a la tabla test2

---
+++ Custom Scripts/Pre-Deployment/PreScript.sql
@@ -0,0 +1,9 @@
+/*
+* Use this Pre-Deployment script to perform tasks before the deployment of the project.
+* Read more at https://www.red-gate.com/SOC7/pre-deployment-script-help
+*/
+insert into test2
+(nom)
+values
+('Hola'),
+('Jesús')
