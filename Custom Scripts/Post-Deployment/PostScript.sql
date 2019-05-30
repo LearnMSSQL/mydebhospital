000d91663a3af0d04f003f23562a9fc37efa8516
JesusCanales <jesuscanales@outlook.com>
2019-05-30 02:08:22 -0500

    mas datos para test2

---
+++ Custom Scripts/Post-Deployment/PostScript.sql
@@ -0,0 +1,9 @@
+/*
+* Use this Post-Deployment script to perform tasks after the deployment of the project.
+* Read more at https://www.red-gate.com/SOC7/post-deployment-script-help
+*/
+insert into test2
+(nom)
+values
+('Ana'),
+('Fabiola')
