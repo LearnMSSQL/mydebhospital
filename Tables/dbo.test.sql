a2c4603eaa121f02280699c62e09753bb68d304f
JesusCanales <jesuscanales@outlook.com>
2019-06-07 22:00:16 -0500

    he agregado nombres y apellidos

--- Tables/dbo.test.sql
+++ Tables/dbo.test.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [dbo].[test]
 (
 [cod] [int] NULL,
-[dato] [varchar] (85) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[dato] [varchar] (85) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[nombres] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[apellidos] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
