dc17ea439e6d534295f285423dcb7ad74d76ad0f
JesusCanales <jesuscanales@outlook.com>
2019-06-07 21:45:55 -0500

    agregue un nuevo campo

--- Tables/dbo.dos.sql
+++ Tables/dbo.dos.sql
@@ -1,5 +1,6 @@
 CREATE TABLE [dbo].[dos]
 (
-[texto] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[texto] [varchar] (20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[cantidad] [int] NULL
 ) ON [PRIMARY]
 GO
